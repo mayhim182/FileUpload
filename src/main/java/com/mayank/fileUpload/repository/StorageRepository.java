cedcdc3c4ea687ff4874f0ce3f0e6dcbc0b10c9f
mayhim182 <mayankatnitrkl@gmail.com>
2023-10-10 11:51:00 +0530

    File upload backend

---
+++ src/main/java/com/mayank/fileUpload/repository/StorageRepository.java
@@ -0,0 +1,10 @@
+package com.mayank.fileUpload.repository;
+
+import com.mayank.fileUpload.entity.ImageData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StorageRepository extends JpaRepository<ImageData,Long> {
+  Optional<ImageData> findByName(String fileName);
+}
---
+++ src/main/java/com/mayank/fileUpload/service/StorageClass.java
@@ -0,0 +1,35 @@
+package com.mayank.fileUpload.service;
+
+import com.mayank.fileUpload.entity.ImageData;
+import com.mayank.fileUpload.repository.StorageRepository;
+import com.mayank.fileUpload.util.ImageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Service
+public class StorageClass {
+  @Autowired
+  private StorageRepository repository;
+
+  public String uploadImage(MultipartFile file) throws IOException {
+    ImageData imageData = repository.save(ImageData.builder()
+      .name(file.getOriginalFilename())
+      .type(file.getContentType())
+      .imageData(ImageUtils.compressImage(file.getBytes()))
+      .build());
+    if(imageData!=null){
+      return "file uploaded successfully "+file.getOriginalFilename();
+    }
+    return null;
+  }
+
+  public byte[] downloadImage(String fileName){
+    Optional<ImageData> dbImageData = repository.findByName(fileName);
+    byte[] images = ImageUtils.decompressImage(dbImageData.get().getImageData());
+    return images;
+  }
+}
